349fd6af0b69ce7f1680cc3e8c06a6671e73fd05
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2020-11-01 22:29:22 +0100

    set up eslint

---
+++ config/keys.js
@@ -0,0 +1,2 @@
+const mongoURI = 'mongodb+srv://paul044:.Millicent99@react-course.lorip.mongodb.net/?retryWrites=true&w=majority';
+module.exports = { mongoURI };
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 
-const app = express()
+const { MongoClient } = require('mongodb');
+
+const app = express();
+
+//  DB config
+const { mongoURI } = require('./config/keys');
+
+//  Connect to MongoDB
+const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((err) => {
+  if (err) console.error(err);
+  const collection = client.db('test').collection('devices');
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (request, response) => {
-  response.send('<h1 style="color: maroon; text-align: center; margin: 5rem;">Bad gateway</h1>')
-})
+  response.send('<h1 style="color: maroon; text-align: center; margin: 5rem;">Bad gateway</h1>');
+});
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});

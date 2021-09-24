f7bde5f6b67a3177c1318c2e19eb85de410e704d
Paul Ogwulumba <pauleugeneogwulumba@gmail.com>
2021-09-24 15:51:21 +0100

    Setting up app
    Setting up app dependencies

--- config/keys.js
+++ config/keys.js
@@ -1,2 +1,3 @@
-const mongoURI = 'mongodb+srv://paul044:.Millicent99@react-course.lorip.mongodb.net/?retryWrites=true&w=majority';
+//const mongoURI = 'mongodb+srv://paul044:.Millicent99@react-course.lorip.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = 'mongodb://127.0.0.1:27017'
 module.exports = { mongoURI };
--- server.js
+++ server.js
@@ -10,15 +10,17 @@ const { mongoURI } = require('./config/keys');
 
 //  Connect to MongoDB
 const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect((err) => {
+client.connect(async (err) => {
   if (err) console.error(err);
   const collection = client.db('test').collection('devices');
   // perform actions on the collection object
+  const data = await collection.find().toArray();
+  console.log(data)
   client.close();
 });
 
 app.get('/', (request, response) => {
-  response.send('<h1 style="color: maroon; text-align: center; margin: 5rem;">Bad gateway</h1>');
+  response.send('<h1 style="color: maroon; text-align: center; margin: 5rem;">You have taken a wrong turn</h1>');
 });
 
 const port = process.env.PORT || 5000;

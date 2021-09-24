969d79b25dfc5ec2a781d2aa2b4cfd7f38d9f021
Paul Ogwulumba <pauleugeneogwulumba@gmail.com>
2021-09-24 23:07:58 +0100

    jest integratiom

---
+++ models/User.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+const mongodb = require('mongodb');
+
+console.log(mongodb);
+
+const { mongoURI } = require('../config/keys');
+
+// console.log(Schema);
+
+// Create Schema
+// const UserSchema = 
